d0ea9d2c28c175b35dcf931682731dd77f3ac1af
Wadii Hajji <wadii.hajji@etu.univ-amu.fr>
2020-03-04 16:28:17 +0100

    Checkbox + input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface Option {
+  value: string;
+  selected: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'material-playground';
+  options: Option[] = [
+    { value: 'option1', selected: false },
+    { value: 'option2', selected: false },
+    { value: 'option3', selected: false },
+    { value: 'option4', selected: false },
+  ];
+
+  optionClicked(event: Event) {
+    event.stopPropagation();
+  }
+
+  toggleSelection(option: Option) {
+    console.log(option);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MaterialModule } from './material.module';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    MaterialModule
+    MatSelectModule,
+    MatFormFieldModule,
+    MatCheckboxModule,
+    FormControl,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    MatChipsModule,
+    MatIconModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
